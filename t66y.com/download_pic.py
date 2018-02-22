395b6b528e263c7982e8c180e430c7abf89eaed5
hejiabin <jiabin.he@coconutislandstudio.com>
2018-02-22 18:09:56 +0800

    fix

--- t66y.com/download_pic.py
+++ t66y.com/download_pic.py
@@ -5,9 +5,9 @@ import time
 import MongoStore
 
 
-def download_pic(header, store, url):
+def download_pic(proxy, store, url):
     try:
-        pic_data = requests.get(url, headers= header)
+        pic_data = requests.get(url, proxies= proxy)
 
         if pic_data.status_code == 200:
             # 写数据库
@@ -19,14 +19,13 @@ def download_pic(header, store, url):
 
 
 # 下载所有帖子中图片
-def download_thread_pic(header, store, keyword):
+def download_thread_pic(proxy, store, keyword):
 
     all_threads = store.get_specific_threads(keyword)
 
     try:
         for t in all_threads:
             tid = t["tid"]
-            name = t["name"]
             urls = store.get_thread_pic_urls(tid)
 
             print("download " + tid)
@@ -35,7 +34,7 @@ def download_thread_pic(header, store, keyword):
             for i in range(len(urls)):
                 url = urls[i]
                 if not store.has_pic(url):
-                    download_pic(header, store, url)
+                    download_pic(proxy, store, url)
 
     except BaseException as be:
         print("-------------------")
@@ -54,17 +53,10 @@ if __name__ == "__main__":
         # 数据库
         store = MongoStore.Store("127.0.0.1", 27017, "t66y_pic")
 
-        header = {}
-        header["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-        header["Accept-Encoding"] = "gzip, deflate"
-        header["Accept-Language"] = "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7"
-        header["Cache-Control"] = "max-age=0"
-        header["Connection"] = "keep-alive"
-        header["Upgrade-Insecure-Requests"] = "1"
-        header["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+        proxy = {"http": "http://127.0.0.1:1080", "https": None}
 
         # 下载帖子内图片
-        download_thread_pic(header, store, argv[1])
+        download_thread_pic(proxy, store, argv[1])
 
         print("----------------------")
         print("图片下载完毕")
--- t66y.com/fetch_thread_info.py
+++ t66y.com/fetch_thread_info.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import MongoStore
+import winsound
+import time
 
 
 g_page_url = "http://ac.postcc.us/thread0806.php?fid=16"
@@ -34,6 +36,9 @@ def fetch_thread_info(url, store):
             thread_id = thread_url.split('/')[-1]
             thread_id = thread_id.split('.')[0]
 
+            if thread_id == "read":
+                continue
+
             if not store.has_thread(thread_id):
                 new_thread_ids.append(thread_id)
                 store.add_thread(thread_id, thread_name, thread_url)
@@ -63,7 +68,41 @@ def fetch_pic_info(tid, store):
 
         if len(url_list) > 0:
             store.set_thread_urls(tid, url_list)
-            print( tid + " collect " + str(len(url_list)) + " images.")
+            print(tid + " collect " + str(len(url_list)) + " images.")
+
+
+# 下载一张图片
+def download_pic(store, url):
+    try:
+        pic_data = requests.get(url)
+
+        if pic_data.status_code == 200:
+            # 写数据库
+            store.add_pic(url, pic_data.content)
+            print("download " + url)
+
+    except BaseException as be:
+        print(str(be))
+
+
+# 下载所有帖子中图片
+def download_thread_pic(store, tid):
+    try:
+        urls = store.get_thread_pic_urls(tid)
+
+        print("download " + tid)
+
+        # 有缺文件或者一个都没有
+        for i in range(len(urls)):
+            url = urls[i]
+            if not store.has_pic(url):
+                download_pic(store, url)
+
+    except BaseException as be:
+        print("-------------------")
+        print(str(be))
+        time.sleep(1.0)
+        winsound.Beep(3500, 1500)
 
 
 def main():
@@ -83,7 +122,9 @@ def main():
         if len(new_thread_ids) == 0:
             break
         else:
-            all_new_thread_ids.append(new_thread_ids)
+            if len(new_thread_ids) > 0:
+                all_new_thread_ids.extend(new_thread_ids)
+
             page_data = requests.get(url)
             if page_data.status_code == 200:
                 page_text = page_data.content.decode("gbk").encode("utf-8")
@@ -97,12 +138,37 @@ def main():
                 break
 
     # 拉帖子内图片信息
+    for tid in all_new_thread_ids:
+        fetch_pic_info(tid, store)
+
+    # 下载帖子内图片数据
+    for tid in all_new_thread_ids:
+        download_thread_pic(store, tid)
+
+
+def test():
+
+    store = MongoStore.Store("127.0.0.1", 27017, "t66y_pic")
+
     all_threads = store.get_all_threads()
 
+    cnt = 0
+    img_cnt = 0
+
     for thread in all_threads:
+        cnt = cnt + 1
+        tid = thread["tid"]
         if "url" not in thread:
-            print( "url not in thread: " + thread["tid"] )
-            fetch_pic_info(thread["tid"], store)
+            print("find no url thread: " + tid)
+            fetch_pic_info(tid, store)
+            download_thread_pic(store, tid)
+        else:
+            urls = thread["url"]
+            url_cnt = len(urls)
+            img_cnt = img_cnt + url_cnt
+
+    print(str(cnt))
+    print("img count: " + str(img_cnt))
 
 
 if __name__ == "__main__":
